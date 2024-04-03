58d0e95efd699232d4103d4b2684126a4a6f27a7
Mihir More <mihirmore.25@gmail.com>
2024-04-03 15:37:52 +0530

    Remove export express validator import statements

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,5 @@
 import express from "express";
 import { register, login, logout } from "../controllers/auth.js";
-import Validate from "../middleware/validate.js";
-import { check } from "express-validator";
 
 const router = express.Router();
 
